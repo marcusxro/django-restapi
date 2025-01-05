5c6ff292935a9f6e47c579e93637ee1d7ab1dba1
marcusxro <marcussalopaso1@gmail.com>
2025-01-05 10:19:59 +0800

    first commit

---
+++ practice/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    fullname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=5, default='USER')
+    
+    def __str__(self):
+        return self.fullname
---
+++ practice/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
+@api_view(['GET'])
+def user_list(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
